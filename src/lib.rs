aca396899543388c35208c9b242c2f0f548dcc76
AmnesiaBeing <727184139@qq.com>
2024-12-19 19:45:55 +0800

    重写测试函数，方便测试

--- src/lib.rs
+++ src/lib.rs
@@ -5,85 +5,103 @@ pub mod types;
 mod tests {
     use crate::{inferences::InferenceSet, types::Field};
 
-    #[test]
-    fn it_works() {
-        println!("begin test.");
+    fn sovle(field: &Field) {
+        let mut field = field.clone();
+        field.print();
+        let ifs = InferenceSet::new();
+        loop {
+            let inference = ifs.analyze(&field);
+            match inference {
+                Some(inference) => {
+                    println!("{:?}", inference);
+                    field = InferenceSet::apply(&field, inference);
+                    field.print();
+                    if field.check_if_finish() {
+                        println!("推导完毕!");
+                        field.print();
+                        // break;
+                    }
+                }
+                None => {
+                    println!("无法推导!");
+                    // break;
+                }
+            }
+        }
+    }
 
-        let field1 = Field::initial_by_string(
+    #[test]
+    fn test1() {
+        let field = Field::initial_by_string(
             &"070009800008002006906100000600000150030801020092000008000003701800600300001900060"
                 .to_string(),
         )
         .unwrap();
+        sovle(&field);
+    }
 
-        // let field2 = Field::initial_by_string(
-        //     &"615800790290600015040000260000080000730512046000090000080000030900008071071060582"
-        //         .to_string(),
-        // )
-        // .unwrap();
+    #[test]
+    fn test2() {
+        let field = Field::initial_by_string(
+            &"615800790290600015040000260000080000730512046000090000080000030900008071071060582"
+                .to_string(),
+        )
+        .unwrap();
+        sovle(&field);
+    }
 
-        // let field3 = Field::initial_by_string(
-        //     &"010076000805000300000000000270000000000500100600000000003000002000900040000000076"
-        //         .to_string(),
-        // )
-        // .unwrap();
+    #[test]
+    fn test3() {
+        let field = Field::initial_by_string(
+            &"010076000805000300000000000270000000000500100600000000003000002000900040000000076"
+                .to_string(),
+        )
+        .unwrap();
+        sovle(&field);
+    }
 
-        // let field4 = Field::initial_by_string(
-        //     &"800000000003600000070090200050007000000045700000100030001000068008500010090000400"
-        //         .to_string(),
-        // )
-        // .unwrap();
+    #[test]
+    fn test4() {
+        let field = Field::initial_by_string(
+            &"800000000003600000070090200050007000000045700000100030001000068008500010090000400"
+                .to_string(),
+        )
+        .unwrap();
+        sovle(&field);
+    }
 
-        // let field5 = Field::initial_by_string(
-        //     &"000000100000500306000000500030600412060300958800000000000000000100000000000000000"
-        //         .to_string(),
-        // )
-        // .unwrap();
+    #[test]
+    fn test5() {
+        let field = Field::initial_by_string(
+            &"000000100000500306000000500030600412060300958800000000000000000100000000000000000"
+                .to_string(),
+        )
+        .unwrap();
+        sovle(&field);
+    }
 
-        // let field6 = Field::initial_by_string(
-        //     &"060000000100000054000000700003000001008010070051000000080900000007100000010000000"
-        //         .to_string(),
-        // )
-        // .unwrap();
+    #[test]
+    fn test6() {
+        let field = Field::initial_by_string(
+            &"060000000100000054000000700003000001008010070051000000080900000007100000010000000"
+                .to_string(),
+        )
+        .unwrap();
+        sovle(&field);
+    }
 
-        let field7 = Field::initial_by_string(
+    #[test]
+    fn test7() {
+        let field = Field::initial_by_string(
             &"586000020020465873437020516300710068008000100010082000073090045000000390090253080"
                 .to_string(),
         )
         .unwrap();
+        sovle(&field);
+    }
 
-        fn sovle(field: &Field) {
-            let mut field = field.clone();
-            field.print();
-            let ifs = InferenceSet::new();
-            loop {
-                let inference = ifs.analyze(&field);
-                match inference {
-                    Some(inference) => {
-                        println!("{:?}", inference);
-                        field = InferenceSet::apply(&field, inference);
-                        field.print();
-                        if field.check_if_finish() {
-                            println!("推导完毕!");
-                            field.print();
-                            // break;
-                        }
-                    }
-                    None => {
-                        println!("无法推导!");
-                        // break;
-                    }
-                }
-            }
-        }
-
-        sovle(&field1);
-        // sovle(&field2);
-        // sovle(&field3);
-        // sovle(&field4);
-        // sovle(&field5);
-        // sovle(&field6);
-        // sovle(&field7);
-
+    #[test]
+    fn other_test(){
         fn generate_combinations(
             len: usize,
             size: usize,
